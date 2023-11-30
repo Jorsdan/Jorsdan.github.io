55f6760b14c33626bfc6bfa26fa377446252de5d
Jorsdan <a82782397@gmail.com>
2023-12-01 00:12:29 +0800

    Site updated: 2023-12-01 00:12:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/27/hello-world/","2023/11/30/test/"];function toRandomPost(){
+var posts=["2023/11/30/test/","2023/11/27/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
